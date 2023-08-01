a227d991477be8e5c5ed0e8e94c211b5482b9086
Alvaro8317 <eduardo831_@hotmail.com>
2023-08-01 12:11:43 -0500

    feat: Added methods to get, post, put and delete requests

--- persistence/database.py
+++ persistence/database.py
@@ -1,15 +1,29 @@
 from pymongo import MongoClient
 from configuration import *
 
+
 class DatabaseConnection:
     MONGODB_URI = f"mongodb+srv://{db_user}:{db_password}@{db_cluster}/?retryWrites=true&w=majority"
     client = None
+
     def __init__(self) -> None:
         pass
 
     def connect(self):
         self.client = MongoClient(self.MONGODB_URI)
         print("Connected")
+
+    def get_context(self):
+        return "Take the context"
+
+    def post_a_context(self):
+        return "Posted"
+
+    def update_a_context(self, id):
+        return f"Updated the context with the id: {id}"
+    
+    def delete_a_context(self, id):
+        return f"Deleted the context with the id: {id}"
 
     def show_databases(self):
         for db_name in self.client.list_database_names():
